f47d7aade09adfc8c30b13f80f325ca2e75428d9
sunny gupta <sunnykr7506@gmail.com>
2024-04-27 20:15:42 +0530

    Frontend Done:)

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+import cors from "cors";
+const app = express()
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
+
+ app.use(cors());
+ app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT ||4000;
+const URI=process.env.MogoDBURI;
+//connect to mongoDB
+try {
+    mongoose.connect(URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+
+    });
+    console.log("connected to mongoDB");
+} catch (error) {
+    console.log("error:", error)
+}
+//routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+  console.log(`server is  listening on PORT ${PORT}`)
+})
