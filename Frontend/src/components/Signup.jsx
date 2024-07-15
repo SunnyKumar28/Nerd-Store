7036ab0857aa9972c3a97b0dc74f904143f54de3
sunny kumar <128737486+SunnyKumar28@users.noreply.github.com>
2024-07-15 18:15:51 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,9 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      // .post("http://localhost:4001/user/signup", userInfo)
+       .post("https://nerd-store-backend.onrender.com/user/signup", userInfo)
+
       .then((res) => {
         console.log(res.data);
         if (res.data) {
